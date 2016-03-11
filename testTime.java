8b43427eaa057b4037534b5b95dd7d6cbe6b68c8
thePledge33 <zgyhlmk@163.com>
2016-03-11 13:23:39 +0800

    Revert "Revert "change test""
    This reverts commit 43a202716dff81947be381510d14c51c0605b6af.

--- testTime.java
+++ testTime.java
@@ -1,7 +1,9 @@
 class testTime{
 	public static void main(String args[]){
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");  
-		 long start = System.currentTimeMillis();  
+		 long start = System11.currentTimeMillis();  
+		 System.out.println(sdf.format(start));  
+		 System.out.println(sdf.format(start));  
 		 System.out.println(sdf.format(start));  
 	}
 }
